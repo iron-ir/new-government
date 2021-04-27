f0bbc296ca85e30c7ade5f22575e0aa55d634f75
nvd <navidsoleymani@ymail.com>
2021-04-27 10:51:35 +0430

    delete test files

--- mysite/users/repository/uploader.py
+++
@@ -1,8 +0,0 @@
-from uuid import uuid4
-
-
-def image_upload_to(instance, file_name):
-    _uuid = uuid4()
-    _uuid2 = uuid4()
-    ext = file_name.split('.')[-1]
-    return "images/{}/{}.{}".format(str(_uuid), str(_uuid2), ext)
