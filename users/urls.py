218ca81a1c507749900a81e0a5b1ab2075a84c42
nvd <navidsoleymani@ymail.com>
2021-05-04 18:39:57 +0430

    add user apis

--- users/urls.py
+++ users/urls.py
@@ -1,23 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('home', views.home, name='home'),
-    path('', views.index, name='index'),
-    path('signup', views.singup_form, name='signup_form'),
-    path('signup/done', views.singup_done, name='signup_done'),
+    path('api/', include('users.api.urls')),
 
-    # path('vcode', views.vcode_form, name='vcode_form'),
-    # path('vcode/done', views.vcode_done, name='vcode_done'),
-
-    path('login', views.login_form, name='login_form'),
-    path('login/done', views.login_done, name='login_done'),
-
-    path('logout', views.logout_form, name='logout_form'),
-    path('logout/done', views.logout_done, name='logout_done'),
-
-    # path('password/reset', views.password_reset_form, name='password_reset_form'),
-    # path('password/reset/done', views.password_reset_done, name='password_reset_done'),
 ]
