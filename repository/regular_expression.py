2da6fb08bfcb56b4fb8f03736b65246ceec731c8
nvd <navidsoleymani@ymail.com>
2021-05-04 18:35:12 +0430

    create re for password

--- repository/regular_expression.py
+++ repository/regular_expression.py
@@ -3,7 +3,7 @@ from django.core import validators
 from django.utils.deconstruct import deconstructible
 
 _username_regular_expression = r'^.{3,20}$'
-_password_regular_expression = r'^.{8,20}$'
+_password_regular_expression = r'^[A-Za-z0-9!@#$&*_.]{8,20}$'
 _phonenumber_regular_expression = r'^(|0|\+98)9[0-9]{9}$'
 _nationalcode_regular_expression = r'^[0-9]{10}$'
 _postalcode_regular_expression = r'^[0-9]{10}$'
@@ -13,6 +13,15 @@ password_reg = re.compile(_password_regular_expression)
 phonenumber_reg = re.compile(_phonenumber_regular_expression)
 nationalcode_reg = re.compile(_nationalcode_regular_expression)
 postalcode_reg = re.compile(_postalcode_regular_expression)
+
+
+@deconstructible
+class UnicodePasswordValidator(validators.RegexValidator):
+    regex = _password_regular_expression
+    message = "The password is only allowed in combination of eight to twenty characters from" \
+              " the following characters.\n- All lowercase English letters\n- All English uppercase" \
+              " letters\n- All English numbers\n- Characters! @ # $ & * _."
+    flags = 0
 
 
 @deconstructible
