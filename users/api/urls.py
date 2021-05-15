7f34e4d8b65cc9598b29eff00dbb129db329e1a2
nvd <navidsoleymani@ymail.com>
2021-05-15 19:44:39 +0430

    change api.urls.py

--- users/api/urls.py
+++ users/api/urls.py
@@ -6,20 +6,21 @@ from . import routes
 app_name = 'users'
 
 urlpatterns = [
-    path('signup', routes.signup_route),
-    path('login', routes.login_route),
-    path('logout', routes.logout_route),
-    path('update', routes.update_route),
-    path('sms/vcode', routes.sms_vcode_route),
-    path('email/vcode', routes.email_vcode_route),
-    path('confirm/phone/number', routes.confirm_phone_number_route),
-    path('confirm/email/address', routes.confirm_email_address_route),
-    path('get/all/user/information', routes.get_all_user_information_route),
-    path('add/work/expiration', routes.add_work_expiration_route),
-    path('add/education/history', routes.add_education_history_route),
-    path('add/standpoint', routes.add_standpoint_route),
-    path('add/effect', routes.add_effect_route),
-    path('add/user/relation', routes.add_user_relation_route),
+    path('get_users', routes.get_users_route),
+    # path('signup', routes.signup_route),
+    # path('login', routes.login_route),
+    # path('logout', routes.logout_route),
+    # path('update', routes.update_route),
+    # path('sms/vcode', routes.sms_vcode_route),
+    # path('email/vcode', routes.email_vcode_route),
+    # path('confirm/phone/number', routes.confirm_phone_number_route),
+    # path('confirm/email/address', routes.confirm_email_address_route),
+    # path('get/all/user/information', routes.get_all_user_information_route),
+    # path('add/work/expiration', routes.add_work_expiration_route),
+    # path('add/education/history', routes.add_education_history_route),
+    # path('add/standpoint', routes.add_standpoint_route),
+    # path('add/effect', routes.add_effect_route),
+    # path('add/user/relation', routes.add_user_relation_route),
 
     # path('candidate/verify/info/', routes.verification_of_information)
 ]
